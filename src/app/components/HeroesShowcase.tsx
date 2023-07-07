f60278333a6c59d65b3353301c0afea1f12519dc
João Pedro Carvalho <93893533+JohnPetros@users.noreply.github.com>
2023-07-07 15:42:12 +0000

    ✨ feat: Add hero carrousel to HeroesShowcase component

--- src/app/components/HeroesShowcase.tsx
+++ src/app/components/HeroesShowcase.tsx
@@ -1,18 +1,47 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
-import { HeroesCarrousel } from "./HeroesCarrousel";
 import { Link } from "./Link";
 import { ArrowLeft, ArrowRight } from "@phosphor-icons/react";
 import { Hero, heroes } from "@/utils/heroes";
 
 export function HeroesShowcase() {
   const [activeHero, setActiveHero] = useState<Hero>(heroes[0]);
+  const [activeHeroIndex, setActiveHeroIndex] = useState(0);
+
+  function handleHeroButtonClick(heroId: number) {
+    const hero = heroes.find(({ id }) => id === heroId)!;
+    const index = heroes.findIndex(({ id }) => id === heroId);
+
+    setActiveHero(hero);
+    setActiveHeroIndex(index);
+  }
+
+  function handleNextHeroButtonClick() {
+    const nextHeroIndex = activeHeroIndex + 1;
+    const nextHero = heroes[nextHeroIndex];
+
+    if (nextHero.id) {
+      setActiveHero(nextHero);
+      setActiveHeroIndex(nextHeroIndex);
+    }
+  }
+
+  function handlePrevHeroButtonClick() {
+    const prevHeroIndex = activeHeroIndex - 1;
+    const prevHero = heroes[prevHeroIndex];
+
+    if (prevHero.id) {
+      setActiveHero(prevHero);
+      setActiveHeroIndex(prevHeroIndex);
+    }
+  }
 
   return (
     <div>
       <div
-        className={`h-[540px] bg-black/70 bg-[url('/images/${activeHero.cover}')] bg-no-repeat bg-cover bg-center bg-blend-color`}
+        style={{ backgroundImage: `url('/images/${activeHero.cover}')` }}
+        className={`h-[540px] bg-black/70 bg-no-repeat bg-cover bg-center bg-blend-color`}
       >
         <div className="max-w-[1200px] w-full h-full mx-auto flex justify-between items-center p-2">
           <div>
@@ -36,9 +65,9 @@ export function HeroesShowcase() {
                 alt="Spider-man"
               />
             </div>
-            <div className="mt-auto w-max">
+            <div className="mt-auto w-96">
               <div className="p-2 backdrop-blur-xl">
-                <div className="pb-10 px-10 pt-5 border border-gray-200 ">
+                <div className="pb-10 px-6 pt-5 border border-gray-200 ">
                   <small className="text-white/90">
                     {activeHero.civilName}
                   </small>
@@ -58,31 +87,30 @@ export function HeroesShowcase() {
                   </a>
                   <div className="flex gap-7 items-center mt-6">
                     <div className="flex gap-3">
-                      {heroes.map((hero) => (
+                      {heroes.map(({ id }) => (
                         <button
-                          className={`block h-2 rounded-full ${
-                            activeHero.id === hero.id
-                              ? "w-4 bg-red"
-                              : "w-2 bg-white"
+                          className={`block h-2 rounded-full transition-all duration-200 ${
+                            activeHero.id === id ? "w-4 bg-red" : "w-2 bg-white"
                           }`}
+                          onClick={() => handleHeroButtonClick(id)}
                         ></button>
                       ))}
                     </div>
                     <div className="flex gap-7">
-                      <button>
-                        <ArrowLeft
-                          size={20}
-                          className="text-white/40"
-                          weight="bold"
-                        />
+                      <button
+                        disabled={activeHeroIndex === 0}
+                        onClick={handlePrevHeroButtonClick}
+                        className="text-white disabled:text-white/40"
+                      >
+                        <ArrowLeft size={20} weight="bold" />
                       </button>
 
-                      <button>
-                        <ArrowRight
-                          size={20}
-                          className="text-white"
-                          weight="bold"
-                        />
+                      <button
+                        disabled={activeHeroIndex === heroes.length - 1}
+                        onClick={handleNextHeroButtonClick}
+                        className="text-white disabled:text-white/40"
+                      >
+                        <ArrowRight size={20} weight="bold" />
                       </button>
                     </div>
                   </div>
@@ -97,8 +125,13 @@ export function HeroesShowcase() {
         <div className="max-w-[1200px] w-full mx-auto flex items-center justify-between p-6">
           {heroes.map(({ id, image, name }) => (
             <button
-              className={`relative w-12 h-12 rounded-full bg-transparent border border-l-4 border-yellow-500/80 overflow-hidden bg-[url('/images/${activeHero.image}')] bg-top`}
+              className={`relative w-12 h-12 hover:scale-110 transition-transform duration-200 rounded-full bg-transparent border border-l-4 overflow-hidden bg-[url('/images/${
+                activeHero.image
+              }')] bg-top ${
+                activeHero.id === id ? "border-red/70" : "border-yellow-500/80"
+              }`}
               style={{ backgroundSize: "300%" }}
+              onClick={() => handleHeroButtonClick(id)}
             >
               <Image src={`/images/${image}`} fill alt={name} />
             </button>
