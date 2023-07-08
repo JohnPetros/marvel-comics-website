59a25a98090a8306bcc9c218b79052f999671cf9
João Pedro <joaopedro.nc@outlook.com>
2023-07-08 18:15:36 -0300

    ✨ feat: Add external prop to link component

--- src/app/components/Link.tsx
+++ src/app/components/Link.tsx
@@ -1,18 +1,25 @@
 import NextLink from "next/link";
 
-interface LinkProps {
+type LinkProps = {
   path: string;
   name: string;
   isActive?: boolean;
-}
+  isExternal?: boolean;
+};
 
-export function Link({ path, name, isActive = true }: LinkProps) {
+export function Link({
+  path,
+  name,
+  isActive = true,
+  isExternal = true,
+}: LinkProps) {
   return (
     <NextLink
       className={`text-white/90 capitalize px-8 py-2 w-max cursor-pointer hover:text-white/50 transition-colors duration-200 ${
         isActive && "bg-red-600 rounded-tl-3xl rounded-br-3xl"
       }`}
       href={path}
+      target={isExternal ? "_blank" : "_self"}
     >
       {name}
     </NextLink>
