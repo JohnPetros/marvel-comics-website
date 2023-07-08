fa8bfca367af37784ea4e420c84f3d36825856be
João Pedro <joaopedro.nc@outlook.com>
2023-07-07 21:56:49 -0300

    ✨ feat: Add check image availabitability function

---
+++ src/utils/checkImageAvailability.ts
@@ -0,0 +1,6 @@
+import { Comic } from "@/@types/comic";
+import { Series } from "@/@types/series";
+
+export function checkImageAvailability(data: Comic | Series) {
+  return !data.thumbnail.path.includes("image_not_available");
+}
