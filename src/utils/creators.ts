5400e139999aece5d779950a72d8870a8c0e1099
João Pedro <joaopedro.nc@outlook.com>
2023-07-08 18:45:18 -0300

    🚚 Add creators data

---
+++ src/utils/creators.ts
@@ -0,0 +1,32 @@
+export const creators = [
+  {
+    id: 1,
+    name: "Jason Aaron",
+    image: "jason-aaron.jpg",
+  },
+  {
+    id: 2,
+    name: "Saladin Ahmed",
+    image: "saladin-ahmed.jpg",
+  },
+  {
+    id: 1,
+    name: "Tini Howard",
+    image: "tini-howard.jpg",
+  },
+  {
+    id: 1,
+    name: "Ta-Nehisi Coates",
+    image: "ta-nehisi-coates.jpg",
+  },
+  {
+    id: 1,
+    name: "Kelly Thompson",
+    image: "kelly-thompson.jpg",
+  },
+  {
+    id: 1,
+    name: "Nnedi Okorafor",
+    image: "nnedi-okorafor.jpg",
+  },
+];
