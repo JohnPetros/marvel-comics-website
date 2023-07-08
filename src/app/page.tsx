4f7a0c9ce69bf16c9d4c0ee2ad206db76611d237
João Pedro <joaopedro.nc@outlook.com>
2023-07-07 21:59:09 -0300

    ✨ feat: Add comics showcase component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { EventsShowcase } from "./components/EventsShowcase";
 import { HeroesShowcase } from "./components/HeroesShowcase";
 import { SeriesShowcase } from "./components/SeriesShowcase";
+import { ComicsShowcase } from "./components/ComicsShowcase";
 
 export default function Home() {
   return (
     <main className="h-full grid gap-32">
       <EventsShowcase />
       <SeriesShowcase />
+      <ComicsShowcase />
       <HeroesShowcase />
     </main>
   );
