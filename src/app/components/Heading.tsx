43f8ca3354798b9ab90f9f10c1e3a0d9b70b526d
João Pedro <joaopedro.nc@outlook.com>
2023-07-08 16:23:05 -0300

    💫 animate: Heading components

--- src/app/components/Heading.tsx
+++ src/app/components/Heading.tsx
@@ -1,17 +1,37 @@
+"use client";
+import { Variants, motion } from "framer-motion";
+
 type HeadingProps = {
   subtitle: string;
   title: string;
 };
 
 export function Heading({ subtitle, title }: HeadingProps) {
+  const headingVariants: Variants = {
+    offscreen: {
+      opacity: 0,
+      y: 24,
+    },
+    onscreen: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
-    <div>
+    <motion.div
+      variants={headingVariants}
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+      transition={{ duration: 0.4 }}
+    >
       <small className="uppercase block text-sm text-red-600 text-center font-bold">
         {subtitle}
       </small>
       <h2 className="uppercase text-black text-4xl text-center mt-2">
         {title}
       </h2>
-    </div>
+    </motion.div>
   );
 }
