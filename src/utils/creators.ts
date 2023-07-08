ac3257b17faaccf977bc15e2bd1b9a174fcde4ed
João Pedro <joaopedro.nc@outlook.com>
2023-07-08 19:11:38 -0300

    📝 Add links to creators data

--- src/utils/creators.ts
+++ src/utils/creators.ts
@@ -3,30 +3,36 @@ export const creators = [
     id: 1,
     name: "Jason Aaron",
     image: "jason-aaron.jpg",
+    link: "https://marvel.fandom.com/wiki/Jason_Aaron",
   },
   {
     id: 2,
     name: "Saladin Ahmed",
     image: "saladin-ahmed.jpg",
+    link: "https://marvel.fandom.com/wiki/Saladin_Ahmed",
   },
   {
     id: 1,
     name: "Tini Howard",
     image: "tini-howard.jpg",
+    link: "https://marvel.fandom.com/wiki/Tini_Howard",
   },
   {
     id: 1,
     name: "Ta-Nehisi Coates",
     image: "ta-nehisi-coates.jpg",
+    link: "https://marvel.fandom.com/wiki/Ta-Nehisi_Coates",
   },
   {
     id: 1,
     name: "Kelly Thompson",
     image: "kelly-thompson.jpg",
+    link: "https://marvel.fandom.com/wiki/Kelly_Thompson",
   },
   {
     id: 1,
     name: "Nnedi Okorafor",
     image: "nnedi-okorafor.jpg",
+    link: "https://marvel.fandom.com/wiki/Nnedi_Okorafor",
   },
 ];
