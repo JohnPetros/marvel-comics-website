3f3ef2e7e5f8589202fa59632393cb2a4ca2ea27
João Pedro <joaopedro.nc@outlook.com>
2023-07-08 19:39:13 -0300

    💄 style: Add bg img to creators showcase

--- src/app/components/CreatorsShowcase/index.tsx
+++ src/app/components/CreatorsShowcase/index.tsx
@@ -2,13 +2,18 @@ import { creators } from "@/utils/creators";
 import { Heading } from "../Heading";
 import { Paragraph } from "../Paragraph";
 import { Creator } from "./Creator";
+import Image from "next/image";
 
 export function CreatorsShowcase() {
   return (
     <section
       id="creators"
-      className="w-[1200px] max-w-full mx-auto flex flex-col items-center justify-center gap-12"
+      className="w-[1200px] max-w-full mx-auto flex flex-col items-center justify-center gap-12 relative"
     >
+      <div className="w-full h-full absolute left-0 top-0 bottom-0 right-0 opacity-10">
+        <Image src="/images/comic.jpg" alt="" fill />
+      </div>
+
       <div className="grid gap-2">
         <Heading title="Behind the pencils & ink" subtitle="Artist" />
         <Paragraph>
