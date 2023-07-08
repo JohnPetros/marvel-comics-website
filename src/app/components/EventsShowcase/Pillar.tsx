2d921f755999fc2010f83d5d957c339d251a4503
João Pedro <joaopedro.nc@outlook.com>
2023-07-08 16:51:19 -0300

    💫 animation: Add height animation to pillar component

--- src/app/components/EventsShowcase/Pillar.tsx
+++ src/app/components/EventsShowcase/Pillar.tsx
@@ -1,24 +1,36 @@
-type PillarProps = {
-  color: string
-  isSmall?: boolean
-}
+"use client";
+import { Variants, motion } from "framer-motion";
 
-export function Pillar({
-  color,
-  isSmall = false,
-}: PillarProps) {
+type PillarProps = {
+  color: string;
+  isSmall?: boolean;
+};
+
+export function Pillar({ color, isSmall = false }: PillarProps) {
+  const pillarVariants: Variants = {
+    offscreen: {
+      height: 0,
+      opacity: 0,
+    },
+    onscreen: {
+      height: 128,
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+        delay: 0.2,
+      },
+    },
+  };
+
   return (
-    <span
-      className={`${
-        isSmall
-          ? 'h-12'
-          : 'h-32'
-      } w-16 bg-${color} border border-${
-        color ===
-        'yellow-500'
-          ? 'yellow-900'
-          : 'black'
+    <motion.span
+      variants={pillarVariants}
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+      className={`${isSmall ? "h-12" : "h-32"} w-16 bg-${color} border border-${
+        color === "yellow-500" ? "yellow-900" : "black"
       }`}
-    ></span>
-  )
+    ></motion.span>
+  );
 }
