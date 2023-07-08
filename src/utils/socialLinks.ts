2b4f555e47f6d2e97090916bac349c8007126668
João Pedro <joaopedro.nc@outlook.com>
2023-07-08 18:13:42 -0300

    📝 Add social links data

---
+++ src/utils/socialLinks.ts
@@ -0,0 +1,42 @@
+export const socialLinks = [
+  {
+    id: 1,
+    icon: "pinterest.svg",
+    path: "",
+  },
+  {
+    id: 2,
+    icon: "youtube.svg",
+    path: "#",
+  },
+  {
+    id: 3,
+    icon: "twitter.svg",
+    path: "#",
+  },
+  {
+    id: 4,
+    icon: "facebook.svg",
+    path: "#",
+  },
+  {
+    id: 5,
+    icon: "snapchat.svg",
+    path: "#",
+  },
+  {
+    id: 6,
+    icon: "tiktok.svg",
+    path: "#",
+  },
+  {
+    id: 7,
+    icon: "instagram.svg",
+    path: "#",
+  },
+  {
+    id: 8,
+    icon: "tumblr.svg",
+    path: "#",
+  },
+];
