e02379f1358e1cb2893dbb4f303e1a44b8e93cbd
João Pedro <joaopedro.nc@outlook.com>
2023-07-08 18:13:23 -0300

    📝 Add links data

---
+++ src/utils/links.ts
@@ -0,0 +1,42 @@
+export const links = [
+  {
+    id: 1,
+    name: "about Marvel",
+    path: "#",
+  },
+  {
+    id: 2,
+    name: "Help/Faqs",
+    path: "#",
+  },
+  {
+    id: 3,
+    name: "Careers",
+    path: "#",
+  },
+  {
+    id: 4,
+    name: "Interships",
+    path: "#",
+  },
+  {
+    id: 5,
+    name: "Advertising",
+    path: "#",
+  },
+  {
+    id: 6,
+    name: "Disney+",
+    path: "#",
+  },
+  {
+    id: 7,
+    name: "Marvelhq.com",
+    path: "#",
+  },
+  {
+    id: 8,
+    name: "Redeem digital comics",
+    path: "#",
+  },
+];
