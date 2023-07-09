be4acbba90d954f336f6731c4bc48ffe299f802b
João Pedro <joaopedro.nc@outlook.com>
2023-07-09 13:48:18 -0300

    🔧 chore: Add xsm breakpoint to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xsm: "440px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       colors: {
         black: "#202020",
