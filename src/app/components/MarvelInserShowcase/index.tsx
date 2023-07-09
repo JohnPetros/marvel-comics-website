e5c498aa9057ec08830e48b9eda4018589d916fd
João Pedro <joaopedro.nc@outlook.com>
2023-07-09 13:17:49 -0300

    📱 Make Marvel insider component responsive

--- src/app/components/MarvelInserShowcase/index.tsx
+++ src/app/components/MarvelInserShowcase/index.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Image from "next/image";
 import { Link } from "../Link";
-import { Variants, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 export function MarvelInserShowcase() {
   return (
     <section className="w-full h-[680px] relative flex flex-col justify-center">
-      <div className="grid grid-cols-[1fr_1.2fr] absolute top-0 left-0 right-0 bottom-0">
+      <div className="sm:grid sm:grid-cols-[1fr_1.2fr] h-[680px] sm:absolute top-0 left-0 right-0 bottom-0">
         <div className=" bg-[#000]"></div>
 
         <motion.div
@@ -20,26 +20,26 @@ export function MarvelInserShowcase() {
         </motion.div>
       </div>
 
-      <motion.div
-        initial={{ opacity: 0, x: -250 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ opacity: { delay: 0.2 }, x: { duration: 0.6 } }}
-        className="z-20 w-full max-w-[1200px] mx-auto"
-      >
-        <small className="uppercase text-sm text-red-600 flex items-center gap-2 font-bold mb-12 after:inline-block after:h-1 after:w-12 after:bg-red-600">
-          Earn rewards
-        </small>
-        <Image
-          src="/icons/marvel-insider.png"
-          width={280}
-          height={280}
-          alt="Marvel Insider"
-        />
-        <p className="text-white/80 my-8">
-          Get rewarded for doing what you already do as a fan.​
-        </p>
-        <Link name="Join Now" path="/" />
-      </motion.div>
+      <div className="z-20 bg-[#000] p-12 sm:bg-transparent sm:p-0">
+        <motion.div
+          initial={{ opacity: 0, x: -250 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ opacity: { delay: 0.2 }, x: { duration: 0.6 } }}
+          className=" w-11/12 max-w-[1200px] mx-auto"
+        >
+          <small className="uppercase text-sm text-red-600 flex items-center gap-2 font-bold after:inline-block after:h-1 after:w-12 after:bg-red-600">
+            Earn rewards
+          </small>
+
+          <div className="relative lg:w-96 lg:h-44 w-64 h-32 mt-8">
+            <Image src="/icons/marvel-insider.png" fill alt="Marvel Insider" />
+          </div>
+          <p className="text-white/80 my-8 w-72 text-left">
+            Get rewarded for doing what you already do as a fan.​
+          </p>
+          <Link name="Join Now" path="/" />
+        </motion.div>
+      </div>
     </section>
   );
 }
