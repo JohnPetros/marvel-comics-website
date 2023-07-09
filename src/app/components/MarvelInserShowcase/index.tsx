5047cf7e60530783113c752cb30ab7a400194674
João Pedro <joaopedro.nc@outlook.com>
2023-07-09 11:11:45 -0300

    💫 Animate marvel insider image

--- src/app/components/MarvelInserShowcase/index.tsx
+++ src/app/components/MarvelInserShowcase/index.tsx
@@ -1,23 +1,31 @@
+"use client";
 import Image from "next/image";
 import { Link } from "../Link";
+import { Variants, motion } from "framer-motion";
 
 export function MarvelInserShowcase() {
   return (
     <section className="w-full h-[680px] relative flex flex-col justify-center">
       <div className="grid grid-cols-[1fr_1.2fr] absolute top-0 left-0 right-0 bottom-0">
-        <div className=" bg-[#000]">
-          <div className=" h-full"></div>
-        </div>
+        <div className=" bg-[#000]"></div>
 
-        <div
+        <motion.div
+          initial={{ opacity: 0, x: 250 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ opacity: { delay: 0.2 }, x: { duration: 0.6 } }}
           style={{ backgroundImage: "url('/images/marvel-insider.jpg')" }}
           className="relative h-full bg-marvel-insider-image bg-no-repeat bg-cover bg-center bg-blend-color"
         >
           <span className="absolute top-0 left-0 right-0 bottom-0 bg-red-600/60"></span>
-        </div>
+        </motion.div>
       </div>
 
-      <div className="z-20 w-full max-w-[1200px] mx-auto">
+      <motion.div
+        initial={{ opacity: 0, x: -250 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ opacity: { delay: 0.2 }, x: { duration: 0.6 } }}
+        className="z-20 w-full max-w-[1200px] mx-auto"
+      >
         <small className="uppercase text-sm text-red-600 flex items-center gap-2 font-bold mb-12 after:inline-block after:h-1 after:w-12 after:bg-red-600">
           Earn rewards
         </small>
@@ -31,7 +39,7 @@ export function MarvelInserShowcase() {
           Get rewarded for doing what you already do as a fan.​
         </p>
         <Link name="Join Now" path="/" />
-      </div>
+      </motion.div>
     </section>
   );
 }
