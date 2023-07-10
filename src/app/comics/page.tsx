db56f68ceae30f4b1989e23525e32bfd82e55e3f
João Pedro <joaopedro.nc@outlook.com>
2023-07-10 16:24:42 -0300

    ♻️ refactor: Move fetch date function to comics page root

--- src/app/comics/page.tsx
+++ src/app/comics/page.tsx
@@ -1,11 +1,23 @@
 import { Header } from "./components/Header";
 import { ComicsList } from "./components/ComicsList";
+import { fetchData } from "@/utils/fetchData";
+import { checkImageAvailability } from "@/utils/checkImageAvailability";
 
-export default function Comics() {
+async function getComics() {
+  const response = await fetchData({ resource: "comics", limit: 100 });
+  const data = response.json();
+  return data;
+}
+
+export default async function Comics() {
+  const response = await getComics();
+  const comics = response.data.results;
+  const availableComics = comics.filter(checkImageAvailability);
+
   return (
     <div className="container mx-auto mt-12 space-y-20">
       <Header />
-      <ComicsList />
+      <ComicsList comics={availableComics} />
     </div>
   );
 }
