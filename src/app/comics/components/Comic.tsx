9eda4889d0d24c581e4ed4adcd0de122be799bd1
João Pedro <joaopedro.nc@outlook.com>
2023-07-10 15:55:50 -0300

    💫 Animate comic card

--- src/app/comics/components/Comic.tsx
+++ src/app/comics/components/Comic.tsx
@@ -1,14 +1,21 @@
-import { Comic } from "@/@types/comic";
+"use client";
 import Image from "next/image";
-
+import { Comic } from "@/@types/comic";
+import { motion } from "framer-motion";
 interface ComicProps {
   data: Comic;
 }
 
 export function Comic({ data }: ComicProps) {
   return (
-    <a href={`comics/${data.id}`} className="flex flex-col items-center gap-2">
-      <div className="relative w-56 h-80">
+    <motion.a
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.25 }}
+      href={`comics/${data.id}`}
+      className="flex flex-col items-center gap-4 group"
+    >
+      <div className="relative w-56 h-80 shadow-xl group-hover:-translate-y-4 transition-transform duration-200">
         <Image
           src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
           alt={data.title}
@@ -16,6 +23,6 @@ export function Comic({ data }: ComicProps) {
         />
       </div>
       <strong>{data.title}</strong>
-    </a>
+    </motion.a>
   );
 }
