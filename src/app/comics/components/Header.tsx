cc7b3682c3b3a14946f81cabff2014026527b901
João Pedro <joaopedro.nc@outlook.com>
2023-07-10 17:01:21 -0300

    💄 styles: Show comics results on the header comics in comics page

--- src/app/comics/components/Header.tsx
+++ src/app/comics/components/Header.tsx
@@ -2,14 +2,16 @@
 import { Heading } from "@/app/components/Heading";
 import { Link } from "@/app/components/Link";
 import { Search } from "@/app/components/Search";
+import { useComics } from "@/app/hooks/useComics";
 
 export function Header() {
   const date = new Date();
   const mouth = date.getMonth();
   const today = date.getDate();
+  const { visibleComics, setVisibleComics } = useComics();
 
   return (
-    <div className="container mx-auto space-y-12">
+    <div className="container mx-auto space-y-6">
       <div className="flex items-center justify-between">
         <Heading title="New this week" subtitle={`On sale ${today}/${mouth}`} />
         <nav className="flex gap-3">
@@ -21,6 +23,14 @@ export function Header() {
       </div>
 
       <Search />
+      <div className="flex justify-between">
+        <span className="uppercase text-gray-400 text-sm font-bold">
+          {visibleComics?.length} results
+        </span>
+        <span className="uppercase text-gray-400 text-sm font-bold">
+          sort by
+        </span>
+      </div>
     </div>
   );
 }
