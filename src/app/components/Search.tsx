42a9febef5f459c129ccd4137787956e6d32a81f
João Pedro <joaopedro.nc@outlook.com>
2023-07-10 15:44:03 -0300

    💄 style: Search component

---
+++ src/app/components/Search.tsx
@@ -0,0 +1,21 @@
+import { MagnifyingGlass } from "@phosphor-icons/react";
+
+export function Search() {
+  return (
+    <div className="border-b-2 border-black flex items-center group focus-within:border-red-600">
+      <label htmlFor="search">
+        <MagnifyingGlass
+          size={24}
+          weight="bold"
+          className="text-black group-focus-within:text-red-600"
+        />
+      </label>
+      <input
+        type="search"
+        id="search"
+        className="text-black p-2 uppercase w-full bg-transparent outline-none font-semibold "
+        placeholder="search"
+      />
+    </div>
+  );
+}
