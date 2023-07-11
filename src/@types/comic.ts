dded55ca18b195a117470d36d2b53505541c1f9f
João Pedro <joaopedro.nc@outlook.com>
2023-07-11 14:19:01 -0300

    🏷️ Add order and comic category type

--- src/@types/comic.ts
+++ src/@types/comic.ts
@@ -1,7 +1,11 @@
 import { Thumbnail } from "./thumbnail";
 
+export type Order = "asc" | "desc";
+
+export type Category = "comics" | "series" | "events";
+
 export type Comic = {
   id: number;
   title: string;
   thumbnail: Thumbnail;
-};  
+};
