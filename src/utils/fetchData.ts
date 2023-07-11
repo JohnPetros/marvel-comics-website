a700159415ca648c81630c8fcc2fdd7849479976
João Pedro <joaopedro.nc@outlook.com>
2023-07-11 19:05:21 -0300

    ♻️ refactor: Change env variables from sserver scopo to client scopo

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,12 +1,30 @@
-type Resource = "comics" | "characters" | "creators";
+import { Category as ComicCategory } from "@/@types/comic";
 
+type Resource = "characters" | ComicCategory;
 interface fetchDataParams {
   resource: Resource;
-  limit: number;
+  limit?: number;
+  orderParams?: string[];
 }
 
-export async function fetchData({ resource, limit = 20 }: fetchDataParams) {
+function formatOrderParams(orderParam: string) {
+  return "orderBy=" + orderParam;
+}
+
+export async function fetchData({
+  resource,
+  limit = 20,
+  orderParams = [],
+}: fetchDataParams) {
+  const authParams = `ts=${process.env.NEXT_PUBLIC_TIMESTAMP}&apikey=${process.env.NEXT_PUBLIC_PUBLIC_KEY}&hash=${process.env.NEXT_PUBLIC_HASH}`;
+
   return await fetch(
-    `${process.env.BASE_URL}/${resource}?ts=${process.env.TIMESTAMP}&apikey=${process.env.PUBLIC_KEY}&hash=${process.env.HASH}&limit=${limit}`
+    `${
+      process.env.NEXT_PUBLIC_BASE_URL
+    }/${resource}?${authParams}&limit=${limit}${
+      orderParams.length > 0
+        ? "&" + orderParams.map(formatOrderParams).join("&")
+        : ""
+    }`
   );
 }
