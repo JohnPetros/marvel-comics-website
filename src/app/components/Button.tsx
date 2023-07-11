32ebca8973b4750a371ff590c266d638001ae4a1
João Pedro <joaopedro.nc@outlook.com>
2023-07-11 19:22:06 -0300

    💄 style: Add active style to button component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -2,12 +2,15 @@
 interface ButtonProps {
   title: string;
   onClick: VoidFunction;
+  isActive: boolean;
 }
 
-export function Button({ title, onClick }: ButtonProps) {
+export function Button({ title, onClick, isActive = false }: ButtonProps) {
   return (
     <button
-      className="border-2 border-black text-black hover:border-red-600 hover:bg-red-600 hover:text-white transition-colors duration-200 rounded-full py-2 px-6 font-bold uppercase"
+      className={`border-2 hover:border-red-600 hover:bg-red-600 hover:text-white transition-colors duration-200 rounded-full py-2 px-6 font-bold uppercase ${
+        isActive ? "bg-red-600 text-white" : "border-black text-black"
+      }`}
       onClick={onClick}
     >
       {title}
