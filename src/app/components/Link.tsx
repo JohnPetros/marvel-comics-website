40df0c995690c6b20157cddce935b2bd9376dc07
João Pedro <joaopedro.nc@outlook.com>
2023-07-11 19:05:48 -0300

    🐛 fix: Link acitve state bug

--- src/app/components/Link.tsx
+++ src/app/components/Link.tsx
@@ -16,8 +16,8 @@ export function Link({
 }: LinkProps) {
   return (
     <NextLink
-      className={`text-white/90 capitalize px-${
-        isActive ? 8 : 0
+      className={`text-white/90 capitalize ${
+        isActive ? "px-8 " : "px-0"
       } py-2 w-max cursor-pointer hover:text-white/50 transition-colors duration-200 ${
         isActive && "bg-red-600 rounded-tl-3xl rounded-br-3xl"
       }`}
