518e354a06e9ca7c5e1553c926d2a6ab3ffa97e6
João Pedro <joaopedro.nc@outlook.com>
2023-07-12 13:42:36 -0300

    💫 Animate button component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "framer-motion";
 interface ButtonProps {
   title: string;
   onClick: VoidFunction;
@@ -7,13 +8,15 @@ interface ButtonProps {
 
 export function Button({ title, onClick, isActive = false }: ButtonProps) {
   return (
-    <button
+    <motion.button
+      initial={{ opacity: 0, y: 24 }}
+      animate={{ opacity: 1, y: 0 }}
       className={`border-2 hover:border-red-600 hover:bg-red-600 hover:text-white transition-colors duration-200 rounded-full py-2 px-6 font-bold uppercase ${
         isActive ? "bg-red-600 text-white" : "border-black text-black"
       }`}
       onClick={onClick}
     >
       {title}
-    </button>
+    </motion.button>
   );
 }
