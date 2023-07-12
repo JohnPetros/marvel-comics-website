3fcfe01a5cfc0c17f5b78bc7061ae2562b3abbfc
João Pedro <joaopedro.nc@outlook.com>
2023-07-12 16:09:17 -0300

    💄 style: Add composition pattern to link component

--- src/app/components/Link.tsx
+++ src/app/components/Link.tsx
@@ -1,16 +1,17 @@
 "use client";
 import NextLink from "next/link";
+import { ReactNode } from "react";
 
 type LinkProps = {
   path: string;
-  name: string;
+  children: ReactNode;
   isActive?: boolean;
   isExternal?: boolean;
 };
 
 export function Link({
   path,
-  name,
+  children,
   isActive = true,
   isExternal = false,
 }: LinkProps) {
@@ -18,13 +19,13 @@ export function Link({
     <NextLink
       className={`text-white/90 capitalize ${
         isActive ? "px-8 " : "px-0"
-      } py-2 w-max cursor-pointer hover:text-white/50 transition-colors duration-200 ${
+      } py-2  cursor-pointer hover:text-white/50 transition-colors duration-200 ${
         isActive && "bg-red-600 rounded-tl-3xl rounded-br-3xl"
       }`}
       href={path}
       target={isExternal ? "_blank" : "_self"}
     >
-      {name}
+      {children}
     </NextLink>
   );
 }
