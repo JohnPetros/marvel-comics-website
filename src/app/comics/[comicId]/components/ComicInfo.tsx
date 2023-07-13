c2e5af94894ab097850676d5356f15a846f19092
João Pedro <joaopedro.nc@outlook.com>
2023-07-13 10:47:08 -0300

    💫 Animate comic info component

--- src/app/comics/[comicId]/components/ComicInfo.tsx
+++ src/app/comics/[comicId]/components/ComicInfo.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Comic } from "@/@types/comic";
 import Image from "next/image";
 import { Detail } from "./Detail";
 import { formatDate } from "@/utils/formatDate";
+import { Variants, motion } from "framer-motion";
 
 interface ComicInfoProps {
   comic: Comic;
@@ -12,21 +14,76 @@ export function ComicInfo({
 }: ComicInfoProps) {
   const image = `${thumbnail.path}.${thumbnail.extension}`;
 
+  const backgroundVariants: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
+
+  const imageVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      x: -350,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 50,
+        duration: 0.4,
+      },
+    },
+  };
+
+  const detailsVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      scale: 0,
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.2,
+      },
+    },
+  };
   return (
     <div className="w-full py-12 relative bg-black">
-      <div
+      <motion.div
+        variants={backgroundVariants}
+        initial="hidden"
+        animate="visible"
         style={{
           backgroundImage: `url(${image})`,
           backgroundSize: "120%",
         }}
         className=" bg-no-repeat bg-center absolute left-0 top-0 bottom-0 right-0 brightness-[0.1] blur-sm"
-      ></div>
+      />
 
       <div className="container mx-auto grid grid-cols-[350px_550px] gap-16 relative z-50">
-        <div className="relative w-[350px] h-[540px]">
+        <motion.div
+          variants={imageVariants}
+          initial="hidden"
+          animate="visible"
+          className="relative w-[350px] h-[540px] shadow-lg"
+        >
           <Image src={`${image}`} alt={title} fill />
-        </div>
-        <div className="grid grid-cols-1 gap-y-8 content-center">
+        </motion.div>
+        <motion.div
+          variants={detailsVariants}
+          initial="hidden"
+          animate="visible"
+          className="grid grid-cols-1 gap-y-8 content-center"
+        >
           <h2 className="text-white text-3xl font-bold">{title}</h2>
           <div>
             <dl className="grid grid-cols-2 gap-8">
@@ -34,6 +91,7 @@ export function ComicInfo({
                 title="Published"
                 description={formatDate(new Date(dates[1].date))}
               />
+
               {creators.items.map((creator) => (
                 <Detail title={creator.role} description={creator.name} />
               ))}
@@ -42,7 +100,7 @@ export function ComicInfo({
 
             <div className="text-base text-white mt-8">{description}</div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
