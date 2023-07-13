a26e6d384194e643f0194460a8bc76a470a0c525
João Pedro <joaopedro.nc@outlook.com>
2023-07-13 09:33:31 -0300

    🏷️ Add types to comic and creator

--- src/@types/comic.ts
+++ src/@types/comic.ts
@@ -4,8 +4,37 @@ export type Order = "asc" | "desc";
 
 export type Category = "comics" | "series" | "events";
 
-export type Comic = {
+type CreatorItem = {
+  name: string;
+  role: string;
+};
+
+type DateItem = {
+  type: string;
+  date: string;
+};
+
+type PriceItem = {
+  type: string;
+  price: number;
+};
+
+type Object = {
+  [key: string]: any;
+};
+
+export interface Comic extends Object {
   id: number;
   title: string;
   thumbnail: Thumbnail;
-};
+  description: string;
+  dates: DateItem[];
+  creators: { items: CreatorItem[] };
+  issueNumber: number;
+  isbn: number;
+  issn: number;
+  upc: number;
+  pageCount: number;
+  format: string;
+  prices: PriceItem[];
+}
---
+++ src/@types/creator.ts
@@ -0,0 +1,5 @@
+export type Creator = {
+  id: number;
+  name: string;
+  role: string;
+};
