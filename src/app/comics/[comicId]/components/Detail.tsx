503dbc2537a8d74ea509e41fd92973d80df60882
João Pedro <joaopedro.nc@outlook.com>
2023-07-13 10:47:23 -0300

    💫 Animate Detail component

--- src/app/comics/[comicId]/components/Detail.tsx
+++ src/app/comics/[comicId]/components/Detail.tsx
@@ -1,13 +1,26 @@
+"use client";
+import { Variants, motion } from "framer-motion";
 interface DetailProps {
   title: string;
   description: string | number;
 }
 
 export function Detail({ title, description }: DetailProps) {
+  const detailVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      y: 24,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
-    <div className="">
+    <motion.div variants={detailVariants}>
       <dt className="text-white text-xl capitalize font-bold">{title}</dt>
       <dd className="text-white text-md">{description}</dd>
-    </div>
+    </motion.div>
   );
 }
