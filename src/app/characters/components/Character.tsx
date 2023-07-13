050137e76bf30120183e91a89d9abdf44bb32c10
João Pedro <joaopedro.nc@outlook.com>
2023-07-13 16:16:20 -0300

    💄 style: Add scale to image character component

--- src/app/characters/components/Character.tsx
+++ src/app/characters/components/Character.tsx
@@ -16,17 +16,19 @@ export function Character({ data }: CharacterProps) {
           height: "auto",
         }}
       >
-        <div className="relative w-56 h-48 border-b-4 border-red-600">
-          <Image
-            src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
-            alt={data.name}
-            fill
-            className="w-auto h-auto"
-            // placeholder="blur"
-            sizes="(min-width: 1024px) 20vw, (min-width: 640px) 33vw, (min-width: 440px) 50vw, 100vw"
-          />
+        <div className="relative w-56 h-48  border-b-8 border-red-600 overflow-hidden">
+          <div className="absolute top-0 left-0 bottom-0 right-0 group-hover:scale-110 transition-all duration-500">
+            <Image
+              src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
+              alt={data.name}
+              fill
+              className="w-auto h-auto"
+              // placeholder="blur"
+              sizes="(min-width: 1024px) 20vw, (min-width: 640px) 33vw, (min-width: 440px) 50vw, 100vw"
+            />
+          </div>
         </div>
-        <div className="bg-black w-56 h-24 rounded-br-md p-4 relative">
+        <div className="bg-black w-56 h-24 rounded-br-3xl overflow-hidden p-4 relative">
           <span className="absolute h-0 group-hover:h-full z-20 transition-all duration-200 bg-red-600 top-0 right-0 left-0 bottom-0"></span>
           <strong className="text-sm uppercase text-white w-full z-30 relative">
             {data.name}
