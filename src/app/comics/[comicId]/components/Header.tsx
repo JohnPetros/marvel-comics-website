169e4a0c452d4ea7f5ccb4de97e7ef8e371ae9bd
João Pedro <joaopedro.nc@outlook.com>
2023-07-14 15:28:18 -0300

    💄 style: Add comic Header component styles

---
+++ src/app/comics/[comicId]/components/Header.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Category } from "@/@types/comic";
+import { ArrowLeft } from "@phosphor-icons/react";
+import Image from "next/image";
+
+interface HeaderProps {
+  comicCategory: Category;
+}
+
+export function Header({ comicCategory }: HeaderProps) {
+  return (
+    <div className="bg-zinc-900">
+      <div className="container mx-auto flex justify-between">
+        <button className="flex gap-2 text-sm items-center text-white py-2 px-2 hover:bg-white/30">
+          <ArrowLeft
+            width={20}
+            height={20}
+            className="text-red-600"
+            weight="bold"
+          />
+          All {comicCategory}
+        </button>
+
+        <div className="flex gap-6">
+          <button>
+            <Image src="/icons/facebook.svg" width={18} height={18} alt="" />
+          </button>
+          <button>
+            <Image src="/icons/twitter.svg" width={18} height={18} alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
