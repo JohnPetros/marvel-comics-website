ba5724d226afbc43409d7097791f2f9a9ffc5250
João Pedro <joaopedro.nc@outlook.com>
2023-07-13 22:50:19 -0300

    🚚 chore Make ComicDetails as a server component

--- src/app/comics/[comicId]/page.tsx
+++ src/app/comics/[comicId]/page.tsx
@@ -1,30 +1,40 @@
 "use client";
-import { useComic } from "@/hooks/useComic";
 import { ComicInfo } from "./components/ComicInfo";
 import { ComicMoreDetails } from "./components/ComicMoreDetails";
 import { RelatedResourcers } from "./components/RelatedResources";
 import { Category } from "@/@types/comic";
+import { getComic } from "@/utils/getComic";
 
 type Params = {
   comicId: number;
 };
 
+type SearchParams = {
+  category: Category;
+};
+
 interface ComicDetailsProps {
   params: Params;
+  searchParams: SearchParams;
 }
 
-export default function ComicDetails({ params }: ComicDetailsProps) {
-  const category = localStorage.getItem(
-    "marvel-website@comic_category"
-  ) as Category;
-  const { comic } = useComic({ category, id: params.comicId });
+export default async function ComicDetails({
+  params,
+  searchParams,
+}: ComicDetailsProps) {
+  const { category } = searchParams;
+  const response = await getComic({
+    category,
+    id: params.comicId,
+  });
+  const comic = response.data.results[0];
 
   return (
     <div>
-      {comic && (
+      {comic.id && (
         <>
           <ComicInfo comic={comic} category={category} />
-          <ComicMoreDetails comic={comic} />
+          {category !== "series" && <ComicMoreDetails comic={comic} />}
           {category !== "comics" && (
             <RelatedResourcers
               originalResourceId={comic.id}
