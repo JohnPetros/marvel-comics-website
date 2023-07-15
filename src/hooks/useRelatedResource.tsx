2bda646965f863dff1d5a756446f2f74fb691900
João Pedro <joaopedro.nc@outlook.com>
2023-07-15 18:00:12 -0300

    ✨ feat: Add pagination to related resources

--- src/hooks/useRelatedResource.tsx
+++ src/hooks/useRelatedResource.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 import { ApiResponse } from "@/@types/apiResponse";
 import { Resource } from "@/@types/resource";
@@ -7,45 +7,81 @@ import { Character } from "@/@types/character";
 import { Comic } from "@/@types/comic";
 
 import { fetchData } from "@/utils/fetchData";
+import { useMemo } from "react";
 
-interface RelatedResourceParams {
+type ResourceData = Comic | Character;
+
+interface getRelatedResourcesParams {
   originalResource: Resource;
   originalResourceId: number;
   relatedResource: Resource;
+  offset: number;
 }
 
 async function getRelatedResources({
   originalResource,
   originalResourceId,
   relatedResource,
-}: RelatedResourceParams): Promise<ApiResponse<Comic | Character>> {
+  offset,
+}: getRelatedResourcesParams): Promise<ApiResponse<ResourceData>> {
   const response = await fetchData({
     resource: `${originalResource}/${originalResourceId}/${relatedResource}`,
+    offset,
   });
 
-  console.log(`${originalResource}/${originalResourceId}/${relatedResource}`);
-
   const data = response.json();
-
   return data;
+}
+
+interface useRelatedResourcesParams {
+  originalResource: Resource;
+  originalResourceId: number;
+  relatedResource: Resource;
 }
 
 export function useRelatedResource({
   originalResource,
   originalResourceId,
   relatedResource,
-}: RelatedResourceParams) {
-  const { data: response, isLoading } = useQuery(
+}: useRelatedResourcesParams) {
+  const {
+    data: response,
+    error,
+    isLoading,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
     ["relatedResource", originalResource, relatedResource],
-    () =>
-      getRelatedResources({
+    ({ pageParam = 0 }) => {
+      return getRelatedResources({
         originalResource,
         originalResourceId,
         relatedResource,
-      })
+        offset: pageParam * 20,
+      });
+    },
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.data.results.length ? allPages.length + 1 : undefined;
+      },
+    }
   );
 
-  const resourcesData = response ? response.data.results : [];
+  const resourcesData: ResourceData[] = useMemo(() => {
+    if (!response?.pages || response.pages[0].code === "RequestThrottled") {
+      return [];
+    }
 
-  return { resourcesData, isLoading };
+    return response.pages.reduce<ResourceData[]>(
+      (allResourcesData, currentPage) => {
+        const resourcesData = currentPage.data.results;
+
+        return [...allResourcesData, ...resourcesData];
+      },
+      []
+    );
+  }, [isFetching]);
+
+  return { resourcesData, isLoading, isFetching, fetchNextPage, hasNextPage };
 }
