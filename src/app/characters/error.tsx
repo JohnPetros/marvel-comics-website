5646c77234ad337da87cca78f01dd99d0da98a84
João Pedro <joaopedro.nc@outlook.com>
2023-07-16 14:46:14 -0300

    🔒 Add error handler to characters page

---
+++ src/app/characters/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { ErrorComponent } from "../components/ErrorComponent";
+
+interface ErrorProps {
+  error: Error;
+}
+
+export default function Error({ error }: ErrorProps) {
+  return (
+    <ErrorComponent
+      title={`404 No characters found`}
+      message="Try to click on the Uatu's eye to go back"
+      handler={null}
+    />
+  );
+}
